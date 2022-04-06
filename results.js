8fea91621a55ab20176ae41439256f2514c5f44b
Dassah-Felix <dassahfelix12@gmailcom>
2022-04-06 14:46:13 +0000

    first commit

---
+++ results.js
@@ -0,0 +1,14 @@
+const results=[
+{name:"Dakurah Stephen", class:"Form One", marks:8},
+{name:"Naafuretobo Francis", class:"Form One", marks:6},
+{name:"Dari Joe", class:"Form One", marks:5},
+{name:"Abdul Raqeeb", class:"Form One", marks:0},
+{name:"Dumba Aisha", class:"Form One", marks:4},
+{name:"Naa Theresa", class:"Form One", marks:8},
+{name:"Bossu Paul", class:"Form One", marks:9},
+{name:"Jamaah Lambo", class:"Form One", marks:10},
+{name:"Naahuu Jamal", class:"Form One", marks:7},
+{name:"Miss Call", class:"Form One", marks:1}
+]
+
+module.exports=results
